59f535c2399b2397d8815c30a1b6a4eb48c45150
Oleksandr <oleksandr@elements.nl>
2018-12-12 09:11:55 +0100

    Add GitHub authentication

--- django_allauth/urls.py
+++ django_allauth/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', views.Home.as_view(), name='home'),
 ]
